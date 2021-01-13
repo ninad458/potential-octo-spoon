3c00002a88781c51ba273d9af5e020951c9bfbff
Ninad Thakare <enigma@root.local>
2021-01-13 18:19:24 +0530

    clean up code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,57 +5,24 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
-import java.io.File
-import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mView = findViewById<ImageView>(R.id.image)
-        val takeScreenshotOfPost = takeScreenshotOfPost(mView)
-        Log.d("zzzzzz", "onCreate: $takeScreenshotOfPost")
-        mView.post {
-//            findViewById<ImageView>(R.id.copy).setImageBitmap(loadBitmapFromView(mView))
-            findViewById<ImageView>(R.id.copy).setImageBitmap(
-                concatDrawable(
-                    loadBitmapFromView(
-                        mView
-                    ), this
-                )
-            )
+        val view = findViewById<ImageView>(R.id.image)
+        val copyImage = findViewById<ImageView>(R.id.copy)
+
+        view.post {
+            val concatDrawable = concatDrawable(loadBitmapFromView(view), this)
+            copyImage.setImageBitmap(concatDrawable)
         }
     }
-}
-
-fun takeScreenshotOfPost(mView: View?): Uri? {
-    val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/DumDum"
-    val dir = File(folderPath)
-    if (!dir.exists()) {
-        dir.mkdirs()
-    }
-    val path = dir.absolutePath + "/invite" + ".png"
-    mView?.isDrawingCacheEnabled = true
-    val bitmap: Bitmap? = Bitmap.createBitmap(mView?.drawingCache ?: return null)
-    mView.isDrawingCacheEnabled = false
-    val imageFile = File(path)
-    val outputStream = FileOutputStream(imageFile)
-    val quality = 100
-    bitmap?.compress(Bitmap.CompressFormat.PNG, quality, outputStream)
-    outputStream.flush()
-    outputStream.close()
-
-    return FileProvider.getUriForFile(mView.context, "com.example.myapplication", File(path))
-
 }
 
 fun loadBitmapFromView(v: View): Bitmap {
@@ -84,7 +51,7 @@ fun concatDrawable(originalBitmap: Bitmap, context: Context): Bitmap {
     )
     val comboImage = Canvas(b)
 
-    drbl.setBounds(0, /*orignalBitmap.height*/0, height, width)
+    drbl.setBounds(0, 0, height, width)
 
     val topImage = Bitmap.createScaledBitmap(tpImg, width, tpImg.height * ratio, true)
 
